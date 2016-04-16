293ffd6c15e685f016482d8d61c80b037d4e4a3a
luismartinez24 <luismartinez242011@gmail.com>
2016-04-16 15:50:30 -0600

    feat: Create polls

--- app/controllers/api/v1/my_polls_controller.rb
+++ app/controllers/api/v1/my_polls_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::MyPollsController < ApplicationController
+	before_action :authenticate , only: [:create, :update, :destroy]
+
 	def index
 		@polls = MyPoll.all
 	end
-	def create
+	def show
 		@poll = MyPoll.find(params[:id])
+	end
+	def create
+		@poll = @current_user.my_polls.new(my_polls_params)
+		if @poll.save
+			render "api/v1/my_polls/show"
+		else
+			render json: {errors: @polls.errors.full_message, status: :unprocessable_entity}
+	    end
 	end
 	def update
 		
@@ -11,4 +21,10 @@ class Api::V1::MyPollsController < ApplicationController
 	def destroy
 		
 	end 
+
+	private
+
+	def my_polls_params
+		params.require(:poll).permit(:title, :description, :expires_at)	
+	end
 end
--- app/models/my_poll.rb
+++ app/models/my_poll.rb
@@ -1,7 +1,9 @@
 class MyPoll < ActiveRecord::Base
+  belongs_to :user
   validates :title, presence: true , length: {  minimum: 10 , message: "%{value} You must have minimum 10 characters"}
   validates :description, presence: true, length: {  minimum: 20 , message: "%{value} You must have minimum 20 characters"}
-  belongs_to :user
+  validates :user, presence: true
+  
 
   def is_valid?
   	DateTime.now < self.expires_at
