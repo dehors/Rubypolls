22307aa8ea4b12091c7248799ec54be2d300cda0
luismartinez24 <luismartinez242011@gmail.com>
2016-04-16 12:14:20 -0600

    feat: Error create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,14 @@ class Api::V1::UsersController < ApplicationController
 
 	# POST/users
 	def create
-		@user = User.from_omniauth(params[:auth])
-		@token = @user.tokens.create()	
-		render "api/v1/users/show"
+		
+		if !params[:auth]
+		render json: { error: "Auht param is missing"}
+		else
+			@user = User.from_omniauth(params[:auth])
+			@token = @user.tokens.create()	
+			render "api/v1/users/show"			
+		end
 	end
 
 end
