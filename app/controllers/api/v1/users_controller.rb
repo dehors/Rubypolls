a87b589b238a465ddba40238d2a2fefca219fe59
luismartinez24 <luismartinez242011@gmail.com>
2016-04-16 11:47:36 -0600

    feat: Creat user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,10 @@
 class Api::V1::UsersController < ApplicationController
 
+	# POST/users
+	def create
+		@user = User.from_omniauth(params[:auth])
+		@token = @user.tokens.create()	
+		render "api/v1/users/show"
+	end
+
 end
