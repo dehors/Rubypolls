2eda9db344de6496d786ce5dc767baf6579ed37f
luismartinez24 <luismartinez242011@gmail.com>
2016-04-16 10:22:47 -0600

    feat: Add model token

---
+++ app/models/token.rb
@@ -0,0 +1,20 @@
+class Token < ActiveRecord::Base
+  belongs_to :user
+  before_create :generate_token
+
+  def is_valid?
+  	DateTime.now < self.expires_at
+  end
+  #verifica si no ha expirado
+  private
+
+  def generate_token
+  	begin
+  		self.token = SecureRandom.hex
+  	end while Token.where(token: self.token).any?
+  	#que siga generando tokens hasta que encuentre uno que sea diferente para guardarlo
+  	self.expires_at ||= 1.month.from_now
+  	#expira dentro de un mes a partir de hoy
+  end
+
+end
---
+++ spec/factories/tokens.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :token do
+    expires_at "2016-04-16 09:08:28"
+    user nil
+    token "MyString"
+  end
+end
