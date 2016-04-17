a8fc2ca37502a1b95feed4bb1742e7c8eda54582
luismartinez24 <luismartinez242011@gmail.com>
2016-04-17 16:35:27 -0600

    feat: CORS

--- app/controllers/api/v1/master_api_controller.rb
+++ app/controllers/api/v1/master_api_controller.rb
@@ -1,3 +1,10 @@
 class Api::V1::MasterApiController < ApplicationController
 	layout "api/v1/application"
+
+	after_action :cors_set_access_control_haeders
+	def cors_set_access_control_haeders
+		headers['Access-Control-Allow-Origin'] = "*"
+		headers['Access-Control-Allow-Methods'] = "POST,GET,PUT,DELETE,OPTIONS"
+		headers['Access-Control-Allow-Headers'] = "Origin,Content-Type,Accept,Authorization,Token"
+	end
 end
