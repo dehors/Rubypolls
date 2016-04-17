2c5a8b41d34caac1af7bd6596b47780efa5a7065
luismartinez24 <luismartinez242011@gmail.com>
2016-04-16 20:55:13 -0600

    feat: Create & update question

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -12,16 +12,21 @@ class Api::V1::QuestionsController < ApplicationController
 	def create
 		@question = @poll.questions.new(questions_params)
 		if @question.save
-			render template:"api/v1/questions/show"
+			render template: "api/v1/questions/show"
 		else
 			render json: { error: @question.errors }, status: :unprocessable_entity
 		end
 	end
 	def update
-		
+		if @question.update(questions_params)
+			render template: "api/v1/questions/show"
+		else
+			render json: { error: @question.errors }, status: :unprocessable_entity
+		end
 	end
 	def delete
-		
+		@question.destroy
+		render json: { message: "Se ha eliminado" }
 	end
 
 	private
