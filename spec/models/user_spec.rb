48ec3d030c36db5bc9c03979699f485ba57f6b91
luismartinez24 <luismartinez242011@gmail.com>
2016-05-01 21:54:31 -0600

    feat: Testing user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
-  #it { should_not allow_value("luismartinez242011").for(:email) }
+  it { should_not allow_value("john@doe").for(:email) }
+  it { should allow_value("john@doe.com").for(:email) }
+  it{ should validate_presence_of(:uid) }
+  it{ should validate_presence_of(:provider) }
 end
